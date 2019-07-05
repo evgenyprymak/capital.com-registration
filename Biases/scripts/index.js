874b2cee913ade3927e4b1e31759c7900fd71dc6
evgenyprymak <prymakev@gmail.com>
2019-07-05 16:30:40 +0300

    Add files via upload

---
+++ Biases/scripts/index.js
@@ -0,0 +1,72 @@
+var animation_eq = lottie.loadAnimation({
+  container: document.getElementById('biases-eq'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'biases-eq.json'
+})
+var animation_eq2 = lottie.loadAnimation({
+  container: document.getElementById('biases-eq2'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'biases-eq-2.json'
+})
+
+var animation_1trade = lottie.loadAnimation({
+  container: document.getElementById('biases-1trade'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'biases-1trade.json'
+})
+var animation_1trade2 = lottie.loadAnimation({
+  container: document.getElementById('biases-1trade2'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'biases-1trade2.json'
+})
+var animation_5trades = lottie.loadAnimation({
+  container: document.getElementById('biases-5trades'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'biases-5trades.json'
+})
+var animation_5trades2 = lottie.loadAnimation({
+  container: document.getElementById('biases-5trades2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'biases-5trades2.json'
+})
+var animation_almost = lottie.loadAnimation({
+  container: document.getElementById('biases-almost'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'biases-almost.json'
+})
+var animation_almost2 = lottie.loadAnimation({
+  container: document.getElementById('biases-almost2'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'biases-almost2.json'
+})
+var animation_report = lottie.loadAnimation({
+  container: document.getElementById('biases-report'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'biases-report.json'
+})
+
+var animation_report2 = lottie.loadAnimation({
+  container: document.getElementById('biases-report2'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'biases-report2.json'
+})
